bfe345c7e12880abf4a955f738c4cec164877051
omar <omar@tzst.com>
2024-03-13 16:58:59 +0100

    parte de back end migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
             $table->string('email')->unique();
+            $table->string('Prenom', 30);
+            $table->date('date_naissance');
+            $table->enum('sexe', array (''));
+            $table->string('cin', 8)->unique();
+            $table->string('Image', 150);
+            $table->string('Adress', 150);
+            $table->enum('roles', array ('Surville', 'directeur'));
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
