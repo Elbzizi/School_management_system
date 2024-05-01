fd67182f5fb328d9b3afb60e49c4c28dfde4fc2f
omar <omar@tzst.com>
2024-05-01 13:15:46 +0200

    parte de user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,46 +12,49 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+  /**
+   * The current password being used by the factory.
+   */
+  protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        // $faker = Faker\Factory::create();
-        $sexe = Arr::random(['homme', 'femme']);
-        $role =  'etudiant';
-        // $email = 'user@gmail.com';
-        $photo = 'assets/img/logonull.jpg';
-        return [
-            'name' => fake()->firstName(),
-            'prenom' =>fake()->lastName(),
-            'sexe' => $sexe,
-            'date_naissance' => fake()->date(),
-            'cin' => fake()->numberBetween(1000000, 9999999), // Generates an 8-digit number
-            'photo' =>$photo,
-            'role' => $role,
-            'adress' => fake()->address(),
-            'statut' => 'desactive',
-            'tel' => fake()->phoneNumber(),
-            'email' =>fake()->email(),
-            'password' => 'useruser',
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    // $faker = Faker\Factory::create();
+    $sexe = Arr::random(['homme', 'femme']);
+    $role = 'etudiant';
+    $email = 'user@gmail.com';
+    $photo = 'assets/img/logonull.jpg';
+    return [
+      'name' => fake()->firstName(),
+      'prenom' => fake()->lastName(),
+      'sexe' => $sexe,
+      'date_naissance' => fake()->date(),
+      'cin' => fake()->numberBetween(1000000, 9999999), // Generates an 8-digit number
+      'photo' => $photo,
+      'role' => $role,
+      'adress' => fake()->address(),
+      'statut' => 'desactive',
+      'tel' => fake()->phoneNumber(),
+      // 'email' =>fake()->email(),
+      'email' => $email,
+      'password' => 'useruser',
+    ];
+  }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+  /**
+   * Indicate that the model's email address should be unverified.
+   */
+  public function unverified(): static
+  {
+    return $this->state(fn(array $attributes) => [
+      'email_verified_at' => null,
+    ]);
+  }
 }
+
+
