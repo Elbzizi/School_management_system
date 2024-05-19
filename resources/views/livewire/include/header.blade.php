f8da4725aa1024cd5f3b22d89c90b81f729d9e38
saadZaari <saad55zaari@gmail.com>
2024-05-19 08:33:20 +0000

    ss

--- resources/views/livewire/include/header.blade.php
+++ resources/views/livewire/include/header.blade.php
@@ -29,20 +29,40 @@
     <link rel="stylesheet" href="{{ asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
     <!-- Daterange picker -->
     <link rel="stylesheet" href="{{ asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-    {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    {{-- <script src="https://kit.fontawesome.com/eb54a27021.js" crossorigin="anonymous"></script> --}}
+    <!-- jQuery UI -->
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+
+    {{-- =============================== new link====================================== --}}
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/css/adminlte.min.css') }}">
+    <script src="https://kit.fontawesome.com/eb54a27021.js" crossorigin="anonymous"></script>
     <style>
-        .error{
+
+        .error {
             color: red;
             font-size: 13px;
         }
-        .cmw{
+
+        .cmw {
             max-width: 900px;
 
-            }
+        }
+
         body {
             font-family: "Barlow", sans-serif;
             font-weight: 500;
@@ -59,233 +79,105 @@
             text-decoration: none;
             cursor: pointer;
         }
-.dropdown {
-  width: 150px;
-}
-.dropdown:hover .dropdown-list {
-  opacity: 1;
-  visibility: visible;
-}
-.dropdown-select {
-  border-radius: 4px;
-  background-color: white;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 1.1rem;
-  cursor: pointer;
-}
-.dropdown-list {
-  border-radius: 4px;
-  background-color: white;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  right: 0;
-  opacity: 0;
-  visibility: hidden;
-  transition: opacity 0.2s linear, visibility 0.2s linear;
-  transform: translateY(10px);
-}
-.dropdown-list:before {
-  content: "";
-  width: 100%;
-  height: 10px;
-  background-color: transparent;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transform: translateY(-100%);
-}
-.dropdown-list__item {
-  padding: 1rem;
-  font-size: 1.4rem;
-}
-.radio-inputs {
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  border-radius: 0.5rem;
-  background-color: #EEE;
-  box-sizing: border-box;
-  box-shadow: 0 0 0px 1px rgba(0, 0, 0, 0.06);
-  padding: 0.25rem;
-  width: 270px;
-  font-size: 14px;
-  margin-top: 25px
-}
 
-.radio-inputs .radio {
-  flex: 1 1 auto;
-  text-align: center;
-}
+        .dropdown {
+            width: 150px;
+        }
 
-.radio-inputs .radio input {
-  display: none;
-}
-.radio-inputs label {
-    margin-bottom: 0px;
-}
+        .dropdown:hover .dropdown-list {
+            opacity: 1;
+            visibility: visible;
+        }
 
-.radio-inputs .radio .name {
-  display: flex;
-  cursor: pointer;
-  align-items: center;
-  justify-content: center;
-  border-radius: 0.5rem;
-  border: none;
-  padding: .4rem 0;
-  /* margin-top: 20px */
-  color: rgba(51, 65, 85, 1);
-  transition: all .15s ease-in-out;
-}
+        .dropdown-select {
+            border-radius: 4px;
+            background-color: white;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            font-size: 1.1rem;
+            cursor: pointer;
+        }
 
-.radio-inputs .radio input:checked + .name {
-  background-color: #fff;
-  font-weight: 600;
-}
+        .dropdown-list {
+            border-radius: 4px;
+            background-color: white;
+            position: absolute;
+            top: 100%;
+            left: 0;
+            right: 0;
+            opacity: 0;
+            visibility: hidden;
+            transition: opacity 0.2s linear, visibility 0.2s linear;
+            transform: translateY(10px);
+        }
 
+        .dropdown-list:before {
+            content: "";
+            width: 100%;
+            height: 10px;
+            background-color: transparent;
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translateY(-100%);
+        }
+
+        .dropdown-list__item {
+            padding: 1rem;
+            font-size: 1.4rem;
+        }
+
+        .radio-inputs {
+            position: relative;
+            display: flex;
+            flex-wrap: wrap;
+            border-radius: 0.5rem;
+            background-color: #EEE;
+            box-sizing: border-box;
+            box-shadow: 0 0 0px 1px rgba(0, 0, 0, 0.06);
+            padding: 0.25rem;
+            width: 270px;
+            font-size: 14px;
+            margin-top: 25px
+        }
+
+        .radio-inputs .radio {
+            flex: 1 1 auto;
+            text-align: center;
+        }
+
+        .radio-inputs .radio input {
+            display: none;
+        }
+
+        .radio-inputs label {
+            margin-bottom: 0px;
+        }
+
+        .radio-inputs .radio .name {
+            display: flex;
+            cursor: pointer;
+            align-items: center;
+            justify-content: center;
+            border-radius: 0.5rem;
+            border: none;
+            padding: .4rem 0;
+            /* margin-top: 20px */
+            color: rgba(51, 65, 85, 1);
+            transition: all .15s ease-in-out;
+        }
+
+        .radio-inputs .radio input:checked+.name {
+            background-color: #fff;
+            font-weight: 600;
+        }
     </style>
-    {{-- =============================== new link====================================== --}}
-    <!-- DataTables -->
-    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('assets/plugins/css/adminlte.min.css') }}">
 </head>
 
 
-<script src="https://kit.fontawesome.com/eb54a27021.js" crossorigin="anonymous"></script>
-<style>
-    .error {
-        color: red;
-        font-size: 13px;
-    }
 
-    .cmw {
-        max-width: 900px;
-
-    }
-
-    body {
-        font-family: "Barlow", sans-serif;
-        font-weight: 500;
-        font-style: normal;
-    }
-
-    .button-like-link {
-        background: none;
-        border: none;
-        padding: 0;
-        font: inherit;
-        color: rgb(0, 0, 0);
-        /* or any other link color */
-        text-decoration: none;
-        cursor: pointer;
-    }
-
-    .dropdown {
-        width: 150px;
-    }
-
-    .dropdown:hover .dropdown-list {
-        opacity: 1;
-        visibility: visible;
-    }
-
-    .dropdown-select {
-        border-radius: 4px;
-        background-color: white;
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        font-size: 1.1rem;
-        cursor: pointer;
-    }
-
-    .dropdown-list {
-        border-radius: 4px;
-        background-color: white;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        right: 0;
-        opacity: 0;
-        visibility: hidden;
-        transition: opacity 0.2s linear, visibility 0.2s linear;
-        transform: translateY(10px);
-    }
-
-    .dropdown-list:before {
-        content: "";
-        width: 100%;
-        height: 10px;
-        background-color: transparent;
-        position: absolute;
-        top: 0;
-        left: 0;
-        transform: translateY(-100%);
-    }
-
-    .dropdown-list__item {
-        padding: 1rem;
-        font-size: 1.4rem;
-    }
-
-    .radio-inputs {
-        position: relative;
-        display: flex;
-        flex-wrap: wrap;
-        border-radius: 0.5rem;
-        background-color: #EEE;
-        box-sizing: border-box;
-        box-shadow: 0 0 0px 1px rgba(0, 0, 0, 0.06);
-        padding: 0.25rem;
-        width: 270px;
-        font-size: 14px;
-        margin-top: 25px
-    }
-
-    .radio-inputs .radio {
-        flex: 1 1 auto;
-        text-align: center;
-    }
-
-    .radio-inputs .radio input {
-        display: none;
-    }
-
-    .radio-inputs label {
-        margin-bottom: 0px;
-    }
-
-    .radio-inputs .radio .name {
-        display: flex;
-        cursor: pointer;
-        align-items: center;
-        justify-content: center;
-        border-radius: 0.5rem;
-        border: none;
-        padding: .4rem 0;
-        /* margin-top: 20px */
-        color: rgba(51, 65, 85, 1);
-        transition: all .15s ease-in-out;
-    }
-
-    .radio-inputs .radio input:checked+.name {
-        background-color: #fff;
-        font-weight: 600;
-    }
-</style>
-
-</head>
 
 <body class=" layout-fixed layout-navbar-fixed ">
 
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/css/app.css',
-//                 'resources/js/app.js',
-//             ],
-//             refresh: true,
-//         }),
-//     ],
-// });
-
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import fontAwesome from 'vite-plugin-font-awesome';
 
-export default {
-  plugins: [
-    fontAwesome()
-  ]
-}
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        fontAwesome(),
+    ],
+});
+
 
