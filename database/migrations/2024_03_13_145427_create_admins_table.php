c43fc9d08fb64635a285e536052d7c146604491d
omar <omar@tzst.com>
2024-06-05 22:16:50 +0100

    upload image

--- database/migrations/2024_03_13_145427_create_admins_table.php
+++ database/migrations/2024_03_13_145427_create_admins_table.php
@@ -5,40 +5,39 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('admins', function (Blueprint $table) {
-            Schema::disableForeignKeyConstraints();
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('admins', function (Blueprint $table) {
+      Schema::disableForeignKeyConstraints();
 
-            $table->id();
-            
-            $table->string('name');
-            $table->string('prenom', 30)->nullable();
-            $table->enum('sexe', array ('homme','femme'))->nullable();
-            $table->date('date_naissance')->nullable();
-            $table->string('cin', 8)->unique();
-            $table->string('adress', 150)->nullable();
-            // $table->string('photo', 150)->nullable()->default('assets/img/logonull.jpg');
-            $table->string('photo', 150);
-            $table->enum('role', array ('surveillant', 'directeur','enseignant'))->default('surveillant');
-            $table->enum('statut', array ('active', 'desactive','bloque'))->default('active');
-            $table->string('tel')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+      $table->id();
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('admins');
-    }
+      $table->string('name');
+      $table->string('prenom', 30)->nullable();
+      $table->enum('sexe', array('homme', 'femme'))->nullable();
+      $table->date('date_naissance')->nullable();
+      $table->string('cin', 8)->unique();
+      $table->string('adress', 150)->nullable();
+      $table->string('photo', 150)->nullable()->default('assets/img/logonull.jpg');
+      $table->enum('role', array('surveillant', 'directeur', 'enseignant'))->default('surveillant');
+      $table->enum('statut', array('active', 'desactive', 'bloque'))->default('active');
+      $table->string('tel')->unique();
+      $table->string('email')->unique();
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('password');
+      $table->rememberToken();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('admins');
+  }
 };
